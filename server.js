75735e0655199473d6163428763fc7e979d10237
murcielago17 <murcielago17@hotmail.com>
2015-11-30 07:32:18 -0800

    Require models

--- server.js
+++ server.js
@@ -29,6 +29,47 @@ mongoose.connect('mongodb://localhost/photPlace-app');
 var PhotoComment = require('./models/photoComment'),
     User = require('./models/user');
 
+// middleware for auth
+app.use(cookieParser());
+app.use(session({
+  secret: 'supersecretkey',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// send flash messages
+app.use(flash());
+
+// passport config
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// passport-github config
+passport.use(new GitHubStrategy({
+  clientID: oauth.github.clientID,
+  clientSecret: oauth.github.clientSecret,
+  callbackURL: oauth.github.callbackURL
+}, function (accessToken, refreshToken, profile, done) {
+  User.findOne({ oauthID: profile.id }, function (err, foundUser) {
+    if (foundUser) {
+      done(null, foundUser);
+    } else {
+      var newUser = new User({
+        oauthID: profile.id,
+        username: profile.username
+      });
+      newUser.save(function (err, savedUser) {
+        console.log('saving user...');
+        done(null, savedUser);
+      });
+    }
+  });
+}));
+
+
     // listen on port 3000
 app.listen(3000, function() {
   console.log('server started');
