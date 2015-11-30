ecc86a9e59733c6ada490f0826d21ee7f7295d6c
murcielago17 <murcielago17@hotmail.com>
2015-11-30 07:45:15 -0800

    sanity check -server started- localHost:3000 diplays ok

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var express = require('express'),
     flash = require('express-flash'),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
-    GitHubStrategy = require('passport-github').Strategy,
-    oauth = require('./oauth.js');
+    GitHubStrategy = require('passport-github').Strategy;
+    // oauth = require('./oauth.js');
 
 // configure bodyParser (for receiving form data)
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,45 +29,53 @@ mongoose.connect('mongodb://localhost/photPlace-app');
 var PhotoComment = require('./models/photoComment'),
     User = require('./models/user');
 
-// middleware for auth
-app.use(cookieParser());
-app.use(session({
-  secret: 'supersecretkey',
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+// // middleware for auth
+// app.use(cookieParser());
+// app.use(session({
+//   secret: 'supersecretkey',
+//   resave: false,
+//   saveUninitialized: false
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-// send flash messages
-app.use(flash());
+// // send flash messages
+// app.use(flash());
 
-// passport config
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// // passport config
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
-// passport-github config
-passport.use(new GitHubStrategy({
-  clientID: oauth.github.clientID,
-  clientSecret: oauth.github.clientSecret,
-  callbackURL: oauth.github.callbackURL
-}, function (accessToken, refreshToken, profile, done) {
-  User.findOne({ oauthID: profile.id }, function (err, foundUser) {
-    if (foundUser) {
-      done(null, foundUser);
-    } else {
-      var newUser = new User({
-        oauthID: profile.id,
-        username: profile.username
-      });
-      newUser.save(function (err, savedUser) {
-        console.log('saving user...');
-        done(null, savedUser);
-      });
-    }
-  });
-}));
+// // passport-github config
+// passport.use(new GitHubStrategy({
+//   clientID: oauth.github.clientID,
+//   clientSecret: oauth.github.clientSecret,
+//   callbackURL: oauth.github.callbackURL
+// }, function (accessToken, refreshToken, profile, done) {
+//   User.findOne({ oauthID: profile.id }, function (err, foundUser) {
+//     if (foundUser) {
+//       done(null, foundUser);
+//     } else {
+//       var newUser = new User({
+//         oauthID: profile.id,
+//         username: profile.username
+//       });
+//       newUser.save(function (err, savedUser) {
+//         console.log('saving user...');
+//         done(null, savedUser);
+//       });
+//     }
+//   });
+// }));
+
+// HOMEPAGE ROUTE
+
+app.get('/', function (req, res) {
+    // sanity check
+    res.render('index');
+  // res.render('index', { user: req.user });
+});
 
 
     // listen on port 3000
